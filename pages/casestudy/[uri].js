1cd67ea567b50d85b2fb99e80474f1fa3023ac10
Chris Yerkes <hello@mopsdigital.com>
2022-09-26 14:55:25 +0000

    refactoring case study page template to move graphql query to external lib file.

--- pages/casestudy/[uri].js
+++ pages/casestudy/[uri].js
@@ -1,6 +1,3 @@
-import { client } from '../../lib/apollo';
-import { gql } from '@apollo/client';
-
 import Head from 'next/head';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,22 +5,22 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import TemplateHeader from '../../components/TemplateHeader';
 import ContactInfo from '../../components/ContactInfo';
 import Footer from '../../components/Footer';
+import { getCaseStudy } from '../../lib/individual-case-study';
 
 export default function SlugPage({ caseStudy }) {
 	return (
 		<>
 			<Head>
 				<title>Headless WP Next Starter</title>
-				<link rel='icon' href='favicon.ico'></link>
 			</Head>
 			<TemplateHeader className='position-absolute w-100 top-0 start-0' />
 			<section className='content-section case-study-header hero'>
 				<div className='container'>
 					<div className='row'>
 						<div className='col'>
-							<h2 className='case-study-title'>
+							<h1 className='case-study-title'>
 								{caseStudy?.caseStudyTitle}
-							</h2>
+							</h1>
 							<article
 								className='col-10'
 								dangerouslySetInnerHTML={{
@@ -65,12 +62,22 @@ export default function SlugPage({ caseStudy }) {
 			<section className='content-section project-goals'>
 				<div className='container'>
 					<div className='row'>
-						<div className='col featured-image'>
+						<div className='col project-goals-featured-image'>
 							<img
+								srcSet={caseStudy?.mainImage?.srcSet}
 								src={caseStudy?.mainImage?.sourceUrl}
 								className='img-fluid'
+								alt={caseStudy?.mainImage?.altText}
 							/>
 						</div>
+					</div>
+					<div className='row'>
+						<article
+							className='col-10 project-goals-text'
+							dangerouslySetInnerHTML={{
+								__html: caseStudy?.projectGoals,
+							}}
+						></article>
 					</div>
 				</div>
 			</section>
@@ -81,60 +88,62 @@ export default function SlugPage({ caseStudy }) {
 }
 
 export async function getStaticProps({ params }) {
-	const GET_CASE_STUDY = gql`
-		query GetCaseStudyBySlug($id: ID!) {
-			caseStudy(id: $id, idType: URI) {
-				caseStudyTitle
-				projectMiniDescription
-				introduction
-				industry
-				stack
-				projectLink
-				mainImage {
-					altText
-					srcSet(size: LARGE)
-					sourceUrl
-				}
-				projectGoals
-				projectTechnology
-				imageGallery {
-					altText
-					caption
-					sourceUrl
-					srcSet
-				}
-				developmentProcess
-				finalImage {
-					altText
-					sourceUrl(size: LARGE)
-					srcSet(size: LARGE)
-				}
-				results
-				otherProjects {
-					edges {
-						node {
-							caseStudyTitle
-							featuredImage {
-								node {
-									altText
-									sourceUrl
-									srcSet
-								}
-							}
-							slug
-						}
-					}
-				}
-			}
-		}
-	`;
-	const response = await client.query({
-		query: GET_CASE_STUDY,
-		variables: {
-			id: params.uri,
-		},
-	});
-	const caseStudy = response?.data?.caseStudy;
+	// const GET_CASE_STUDY = gql`
+	// 	query GetCaseStudyBySlug($id: ID!) {
+	// 		caseStudy(id: $id, idType: URI) {
+	// 			caseStudyTitle
+	// 			projectMiniDescription
+	// 			introduction
+	// 			industry
+	// 			stack
+	// 			projectLink
+	// 			mainImage {
+	// 				altText
+	// 				srcSet(size: LARGE)
+	// 				sourceUrl
+	// 			}
+	// 			projectGoals
+	// 			projectTechnology
+	// 			imageGallery {
+	// 				altText
+	// 				caption
+	// 				sourceUrl
+	// 				srcSet
+	// 			}
+	// 			developmentProcess
+	// 			finalImage {
+	// 				altText
+	// 				sourceUrl(size: LARGE)
+	// 				srcSet(size: LARGE)
+	// 			}
+	// 			results
+	// 			otherProjects {
+	// 				edges {
+	// 					node {
+	// 						caseStudyTitle
+	// 						featuredImage {
+	// 							node {
+	// 								altText
+	// 								sourceUrl
+	// 								srcSet
+	// 							}
+	// 						}
+	// 						slug
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// `;
+	// const response = await client.query({
+	// 	query: GET_CASE_STUDY,
+	// 	variables: {
+	// 		id: params.uri,
+	// 	},
+	// });
+	// const caseStudy = response?.data?.caseStudy;
+	const caseStudy = await getCaseStudy(params);
+
 	return {
 		props: {
 			caseStudy,
