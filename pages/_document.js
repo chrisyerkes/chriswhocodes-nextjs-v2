07c6dfac70baeccceb7621b4216ba105c73480c2
Chris Yerkes <hello@mopsdigital.com>
2022-10-24 21:11:38 +0000

    Adding favicon code and graphics.

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+					<link rel="manifest" href="/site.webmanifest"></link>
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+					<meta name="msapplication-TileColor" content="#da532c"></meta>
+					<meta name="theme-color" content="#ffffff"></meta>
 					<link
 						rel='preconnect'
 						href='https://fonts.googleapis.com'
@@ -23,7 +30,7 @@ class MyDocument extends Document {
 						rel='stylesheet'
 						href='https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap'
 					></link>
-					{/* <link rel='icon' href='favicon.ico'></link> */}
+					<link rel='icon' href='favicon.ico'></link>
 				</Head>
 				<body>
 					<Main />
